9db5bef04fec023c8e7e9239aefca24666e4b440
dario <dario@Doku>
2023-03-30 17:25:14 -0400

    ahora si

--- ProyectoFinalPoo/src/logico/Cliente.java
+++ ProyectoFinalPoo/src/logico/Cliente.java
@@ -5,6 +5,37 @@ public class Cliente {
 	private String direccion;
 	private int telefono;
 	private String cedula;
+	public Cliente(String nombre, String direccion, int telefono, String cedula) {
+		super();
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
 	
 	
 }
