837556cc0e394f513a3af20a07899c86c5f6119f
dario <dario@Doku>
2023-04-09 05:13:52 -0400

    mejoras a facturar

--- ProyectoFinalPoo/src/visual/Facturar.java
+++ ProyectoFinalPoo/src/visual/Facturar.java
@@ -285,11 +285,16 @@ public class Facturar extends JDialog {
 								cp.setCantidad(cantidad);
 								if (cantidad > 1) {
 									modelo0.addElement(String.format("%s(%d)", codigo, cantidad));
-									
+
 								}
 							}
 
 							System.out.println(codigo.substring(4, 7));
+
+							// Actualizar modelo de lista 1
+							String codigo2 = (String) modelo.getElementAt(index);
+							String codigoActualizado = String.format("%s(%d)", codigo2.substring(0, 4), cantidad);
+							modelo.setElementAt(codigoActualizado, index);
 						}
 
 						// modelo0 es la lista 2
